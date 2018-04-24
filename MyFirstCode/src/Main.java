06beec2adb0698a94748642f92e455734f9156f0
JOSHUA MUSAU <you@example.com>
2018-04-24 14:25:55 +0200

    Hello World

---
+++ MyFirstCode/src/Main.java
@@ -0,0 +1,24 @@
+
+/*
+ * This is a simple code for printing out the Hello World
+ * @author Joshua Wambua
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * Calls the Print Hello Method
+		 */
+		printHello();
+	}
+
+	private static void printHello() {
+		/**
+		 * Print the given strings on Console
+		 */
+		System.out.println("Hello World\n");
+		System.out.println("This is my First Code !!");
+		
+	}
+
+}
